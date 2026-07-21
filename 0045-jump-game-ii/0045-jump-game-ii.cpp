33beadd1e4cdf054ea5a5ccc7e7980a9dcd8c80f
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-07-21 18:40:51 +0530

    Time: 447 ms (5.03%), Space: 21.9 MB (14.15%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+
+        dp[0]=0;
+        for(int i=1;i<n;i++){
+            dp[i]=INT_MAX;
+            for(int j=0;j<i;j++){
+                if(j+nums[j]>=i){
+                    dp[i]=min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        
+        return dp[n-1];
+        
+    }
+};
