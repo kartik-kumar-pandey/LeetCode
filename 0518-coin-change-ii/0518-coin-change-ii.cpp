5fc4983dcc97081374337aad8bf3c813f94da26c
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-07-31 18:44:01 +0530

    Time: 2 ms (99.18%), Space: 10 MB (96.66%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, int idx, vector<vector<int>>& dp){
-
-        if(amount==0){
-            return 1;
-        }
-
-        if(idx==coins.size()){
-            return 0;
-        }
-
-        if(dp[idx][amount]!=-1){
-            return dp[idx][amount];
-        }
-
-        int ans=solve(coins,amount,idx+1,dp);
-
-        if(amount>=coins[idx]){
-            ans+=solve(coins,amount-coins[idx],idx,dp);
-        }
-
-        return dp[idx][amount]=ans;
-    }
-
     int change(int amount, vector<int>& coins) {
 
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
+        vector<unsigned int> dp(amount + 1, 0);
 
-        return solve(coins,amount,0,dp);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+
+        return dp[amount];
     }
 };
