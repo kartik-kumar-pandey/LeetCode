634de10122b93b3677697d79c94650cfa8e55e8f
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-08-19 16:15:33 +0530

    Time: 0 ms (100%), Space: 72 MB (89.07%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int>v(n,0);
+        vector<int>v(n+1,0);
         for(int i=0;i<bookings.size();i++){
             int a=bookings[i][0];
             int b=bookings[i][1];
             int val=bookings[i][2];
-
-            for(int j=a;j<=b;j++){
-                v[j-1]+=val;
-            }
+            v[a-1]+=val;
+            v[b]-=val;
         }
+        for(int i=1;i<n;i++)
+        {
+            v[i]=v[i-1]+v[i];
+
+        }
+        v.pop_back();
+        
 
         return v;
     }
