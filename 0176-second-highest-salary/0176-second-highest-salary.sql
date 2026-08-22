110bbc833034216dc55e6ad31a8186ec1ab0bed6
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-08-22 14:42:32 +0530

    Time: 3364 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,6 @@
-select Max(salary) as SecondHighestSalary 
-from Employee
-where salary<(select Max(salary) as SecondHighestSalary 
-from Employee);
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
