df098b1849b8b7ea0c68d15fe439e8127e81de23
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-08-22 14:52:21 +0530

    Time: 428 ms (90.63%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,13 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  RETURN (
-      SELECT MAX(SecondHighestSalary) AS SecondHighestSalary
-FROM (
-    SELECT salary AS SecondHighestSalary,
-           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
-    FROM Employee
-) ranked
-WHERE rnk = N
+    SET N = N - 1;
 
-  );
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT N, 1
+    );
 END
