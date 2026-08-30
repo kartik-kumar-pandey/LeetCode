87840410e413586b8b505a31843ffe9d729a2a30
Kartik Kumar Pandey <2k23.csai2313425@gmail.com>
2026-08-30 15:48:23 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.1%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long rev=0;
+        long long temp=x;
+
+        while(temp){
+            rev=rev*10+temp%10;
+            temp/=10;
+        }
+
+        return rev==x;
+    }
+};
